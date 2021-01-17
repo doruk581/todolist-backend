e1888da0a9ffbc26f2382e2fbcf40c794baa11a3
Doruk <doruksucs@gmail.com>
2021-01-17 17:57:44 +0300

    Test jenkins hook

--- src/main/kotlin/com/doruk/todolist/Application.kt
+++ src/main/kotlin/com/doruk/todolist/Application.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class Application
 
+/*Test jenkins hook*/
 object KotlinMain {
     @JvmStatic
     fun main(args: Array<String>) {
